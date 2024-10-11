50284e533507a112b44856f02b369e3f8fd0decc
Danyal Shah <zlac208@live.rhul.ac.uk>
2024-10-11 20:14:04 +0100

    Update Diary. Import base for frontend and backend.

---
+++ backend/src/test/java/danyal/fyp/awd/AdvancedWebDevBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package danyal.fyp.awd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedWebDevBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
