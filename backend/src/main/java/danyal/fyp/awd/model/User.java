bf6785ff96ad6f19c296c19cc52250b0bea15620
Danyal Shah <zlac208@live.rhul.ac.uk>
2024-10-13 23:09:43 +0100

    Refactoring Backend

--- backend/src/main/java/danyal/fyp/awd/model/User.java
+++ backend/src/main/java/danyal/fyp/awd/model/User.java
@@ -14,6 +14,8 @@ import java.util.UUID;
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
+@Getter
+@Setter
 @EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
@@ -32,54 +34,6 @@ public class User {
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreatedDate
     private Instant createdAt;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 
     @Column(name = "updated_at")
     @LastModifiedDate
