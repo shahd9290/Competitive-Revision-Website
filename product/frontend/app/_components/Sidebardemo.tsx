d919435227359e4f9b951db565a3a9d9a1e49be4
Danyal Shah <zlac208@live.rhul.ac.uk>
2024-10-22 13:13:19 +0100

    Sidebar takes up full width of web page

--- product/frontend/app/_components/Sidebardemo.tsx
+++ product/frontend/app/_components/Sidebardemo.tsx
@@ -47,7 +47,7 @@ export function SidebarDemo() {
   return (
     <div
       className={cn(
-        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-screen mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
         "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
       )}
     >
