9dc56d59b959e23a028243dd144d5c2448402067
Danyal Shah <zlac208@live.rhul.ac.uk>
2024-11-28 18:13:26 +0000

    Restructured Bento Grid. Full button triggers dialog box instead of text. Begin work on Dialog Contents.

--- product/frontend/components/ui/bento-grid.tsx
+++ product/frontend/components/ui/bento-grid.tsx
@@ -1,13 +1,6 @@
 import { cn } from "@/lib/utils";
-import {Children} from "react";
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
+import { Children } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 // Bento Grid component made available for public use via Aceternity UI - https://ui.aceternity.com/components/bento-grid
 
@@ -20,13 +13,12 @@ export const BentoGrid = ({
 }) => {
     const childCount = Children.count(children);
 
-
     return (
         <div
             className={cn(
                 childCount <= 4
-                ? "flex items-center justify-center gap-4 lg:gap-20 min-h-[80vh] px-6 py-8"
-                : "grid grid-cols-2 md:grid-cols-4 gap-4 lg:gap-20  max-w-7xl mx-auto px-6 md:px-10 py-14",
+                    ? "flex items-center justify-center gap-4 lg:gap-20 min-h-[80vh] px-6 py-8"
+                    : "grid grid-cols-2 md:grid-cols-4 gap-4 lg:gap-20  max-w-7xl mx-auto px-6 md:px-10 py-14",
                 className
             )}
         >
@@ -43,31 +35,27 @@ export const BentoGridItem = ({
     title?: string | React.ReactNode;
 }) => {
     return (
-        <div
-            className={cn(
-                "aspect-square rounded-xl group/bento hover:shadow-xl transition-all duration-200 shadow-input p-4 " +
-                "bg-[#D9D9D9] border border-transparent justify-center items-center flex flex-col space-y-4",
-                "w-[8rem] h-[8rem] md:w-[10rem] md:h-[10rem]",
-                className
-            )}
-        >
-            <div className="group-hover/bento:scale-110 transition duration-200 text-gray-900">
-                <div className="text-center font-sans font-bold mb-2 mt-2">
-                    <Dialog>
-                        <DialogTrigger>{title}</DialogTrigger>
-                        <DialogContent>
-                            <DialogHeader>
-                                <DialogTitle>Are you absolutely sure?</DialogTitle>
-                                <DialogDescription>
-                                    This action cannot be undone. This will permanently delete your account
-                                    and remove your data from our servers.
-                                </DialogDescription>
-                            </DialogHeader>
-                        </DialogContent>
-                    </Dialog>
-                </div>
-
-            </div>
-        </div>
+        <Dialog>
+            <DialogTrigger asChild>
+                <button
+                    className={cn(
+                        "aspect-square rounded-xl group/bento hover:shadow-xl transition-all duration-200 shadow-input p-4 " +
+                        "bg-[#D9D9D9] border border-transparent justify-center items-center flex flex-col space-y-4",
+                        "w-[8rem] h-[8rem] md:w-[10rem] md:h-[10rem]",
+                        className
+                    )}
+                    type="button"
+                >
+                    <div className="group-hover/bento:scale-110 transition duration-200 text-gray-900">
+                        <div className="text-center font-sans font-bold mb-2 mt-2">
+                            {title}
+                        </div>
+                    </div>
+                </button>
+            </DialogTrigger>
+            <DialogContent>
+                <h1>{title}</h1>
+            </DialogContent>
+        </Dialog>
     );
 };
