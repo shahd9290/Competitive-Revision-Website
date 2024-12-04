519043ee69ab2c713f459253ca4adfdd82859b4e
Danyal Shah <zlac208@live.rhul.ac.uk>
2024-12-04 12:35:30 +0000

    Amend Sidebar Replaced icon. Commented out Profile/Settings for future implementation

--- product/frontend/components/ui/SidebarMenu.tsx
+++ product/frontend/components/ui/SidebarMenu.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
 import {
   IconArrowLeft,
-  IconHome, IconSearch,
+  IconHome, IconSchool, IconSearch,
   IconSettings,
   IconUserBolt,
 } from "@tabler/icons-react";
@@ -29,20 +29,22 @@ export function SidebarMenu(content) {
           <IconSearch className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       )
     },
-    {
-      label: "Profile",
-      href: "#",
-      icon: (
-        <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-      ),
-    },
-    {
-      label: "Settings",
-      href: "#",
-      icon: (
-        <IconSettings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-      ),
-    },
+    // Coming Soon
+    //
+    // {
+    //   label: "Profile",
+    //   href: "#",
+    //   icon: (
+    //     <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    //   ),
+    // },
+    // {
+    //   label: "Settings",
+    //   href: "#",
+    //   icon: (
+    //     <IconSettings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    //   ),
+    // },
     {
       label: "Logout",
       href: "/logout",
@@ -84,12 +86,12 @@ export function SidebarMenu(content) {
 export const Logo = () => {
   return (
       <Link
-          href="#"
+          href="/dashboard"
           className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
       >
-        <div
-            className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
-      <motion.span
+        <IconSchool className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0"/>
+
+        <motion.span
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         className="font-medium text-black dark:text-white whitespace-pre"
@@ -102,10 +104,10 @@ export const Logo = () => {
 export const LogoIcon = () => {
   return (
     <Link
-      href="#"
+      href="/dashboard"
       className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
     >
-      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <IconSchool className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0"/>
     </Link>
   );
 };
