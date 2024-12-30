ee0d41e44dcf9269b70a6161756955f5326c3bf1
Danyal Shah <zlac208@live.rhul.ac.uk>
2024-12-30 12:57:40 +0000

    Id can be processed when accessing quiz page.

--- product/frontend/app/quiz/page.tsx
+++ product/frontend/app/quiz/page.tsx
@@ -1,6 +1,20 @@
+'use client'
 import {SidebarMenu} from "@/components/ui/SidebarMenu";
+import {useRouter} from "next/navigation";
+import React, {useEffect} from "react";
 
-const Quiz = () => {
+interface SearchParams {
+    id?: string;
+}
+
+const Quiz = ({ searchParams }: { searchParams: SearchParams }) => {
+    const {id} = searchParams;
+    useEffect(() => {
+        if (id) {
+            console.log(`Received id: ${id}`);
+        }
+    }, []);
+
     return(
         <div>
             <h1>Quiz Page</h1>
@@ -12,12 +26,12 @@ const Quiz = () => {
 /**
  * A wrapper component for the `Quiz` component with a sidebar menu.
  */
-const Page = () => {
+const Page = ({ searchParams }: { searchParams: SearchParams }) => {
 
     return (
         <div>
             <SidebarMenu>
-                <Quiz/>
+                <Quiz searchParams={searchParams}/>
             </SidebarMenu>
         </div>
     )
