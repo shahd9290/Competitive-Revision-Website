e9c0e257ac0e8acb582d92f1014cf378ae472d1c
Danyal Shah <zlac208@live.rhul.ac.uk>
2025-02-16 14:51:46 +0000

    Add Admin Controller

---
+++ product/backend/src/main/java/danyal/fyp/awd/controller/admin/AdminController.java
@@ -0,0 +1,19 @@
+package danyal.fyp.awd.controller.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Hello Admin");
+    }
+
+}
